cdeb9b585edb0f6c4def9a4e07a1a0717433e640
Nathan Herald <me@nathanherald.com>
2022-03-14 22:33:51 +0100

    Refactor the service worker
    In dev, we go to the network immediately so we can actually see changes in the browser. In prod, we go to cache unless it's HTML, then we async update the cache in the background.

--- public/service-worker.ts
+++ public/service-worker.ts
@@ -4,7 +4,7 @@
 
 declare let self: ServiceWorkerGlobalScope
 
-const cacheName = 'pwaResourcesV5' + (import.meta.env.NODE_ENV || '---')
+const cacheName = 'pwaResourcesV8' + (import.meta.env.NODE_ENV || '---')
 
 self.addEventListener('error', e => {
   console.error('caught unhandled error in service worker', e, e.error, e.filename, e.lineno)
@@ -16,88 +16,112 @@ self.addEventListener('install', e => {
   e.waitUntil(async () => {
     const cache = await caches.open(cacheName)
     cache.add('/')
+    // TODO: how do we know what filenames to cache here?
   })
 })
 
 self.addEventListener('activate', e => {
   self.clients.claim()
+
+  if ('navigationPreload' in self.registration) {
+    // @ts-ignore
+    self.registration.navigationPreload.enable()
+  }
 })
 
 self.addEventListener('fetch', (e: FetchEvent) => {
-  e.respondWith((async () => {
-    console.debug('fetch request', e.request)
+  if (e.request.method !== 'GET') {
+    return
+  }
 
+  e.respondWith((async () => {
     try {
-      if (e.request.method !== 'GET') {
-        return fetch(e.request)
+      if (import.meta.env.NODE_ENV === 'development') {
+        const url = new URL(e.request.url)
+        const fromCache = !!url.searchParams.get('fromcache')
+
+        if (fromCache) {
+          const networkResponse = getFromNetworkAndCache(e)
+          const cachedResponse = await getFromCache(e.request)
+          return cachedResponse || await networkResponse
+        } else {
+          return await getFromNetworkAndCache(e)
+        }
       }
 
-      if (e.request.destination === 'document') {
+      if (isHtmlRequest(e.request)) {
         // NOTE: we always fetch documents in the background to either cache
         // initially or update an existing cache asynchronously
-        const networkResponse = (async () => {
-          const response = await fetch(e.request)
-
-          if (response.ok) {
-            console.debug({ url: e.request.url, putInCache: true, isDocument: true })
-            const cache = await caches.open(cacheName)
-            cache.put(e.request, response.clone())
-          }
-
-          return response
-        })()
-
-        const cachedResponse = await caches.match(e.request, { cacheName })
+        const networkResponse = getFromNetworkAndCache(e)
+        const cachedResponse = await getFromCache(e.request)
 
         if (cachedResponse) {
-          console.debug({ url: e.request.url, fromCache: true, isDocument: true })
+          networkResponse
+            .catch(err => {
+              if (import.meta.env.NODE_ENV === 'development') {
+                console.error('could not fetch and cache a newer version of', e.request.url, err)
+              }
+            })
+
           return cachedResponse
         } else {
-          console.debug({ url: e.request.url, fromNetwork: true, isDocument: true })
-          return networkResponse
+          return await networkResponse
         }
       } else {
         // NOTE: assets are fine to return from cache every time
-        const cachedResponse = await caches.match(e.request, { cacheName })
-        if (cachedResponse) {
-          console.debug({ url: e.request.url, fromCache: true })
-          return cachedResponse
-        }
-
-        const response = await fetch(e.request)
-
-        if (response.ok) {
-          console.debug({ url: e.request.url, putInCache: true })
-          const cache = await caches.open(cacheName)
-          cache.put(e.request, response.clone())
-        }
-
-        return response
+        const cachedResponse = await getFromCache(e.request)
+        if (cachedResponse) { return cachedResponse }
+        return await getFromNetworkAndCache(e)
       }
     } catch (err) {
       console.error('error handling fetch', err)
       console.error(err.stack)
 
-      if (e.request.destination === 'document') {
+      if (isHtmlRequest(e.request)) {
+        // NOTE: fallback to serving the index as our offline page… it should
+        // do client-side routing and work fine…
         return caches.match('/', { cacheName })
       }
 
-      return textResponse('error', 500)
+      return new Response('error', {
+        status: 500,
+        headers: {
+          'Content-Type': 'text/plain',
+          'Cache-Control': 'no-store'
+        }
+      })
     }
   })())
 })
+
+function isHtmlRequest(request: Request): boolean {
+  return request.mode === 'navigate'
+    || request.destination === 'document'
+    || request.headers.get('Accept').includes('text/html')
+}
+
+async function getFromNetworkAndCache(e: FetchEvent): Promise<Response> {
+  // @ts-ignore
+  let response = await e.preloadResponse
+
+  if (!response) {
+    response = await fetch(e.request)
+  }
+
+  if (response.ok) {
+    const cache = await caches.open(cacheName)
+    cache.put(e.request, response.clone())
+  }
+
+  return response
+}
+
+function getFromCache(request: Request): Promise<Response | undefined> {
+  return caches.match(request, { cacheName })
+}
 
 self.addEventListener('message', e => {
   e.waitUntil(async () => {
     console.debug('message', e.data)
   })
 })
-
-function textResponse(text: string, status = 200): Response {
-  return new Response(text, {
-    status,
-    headers: {
-      'content-type': 'text/plain'
-    }
-  })
-}
