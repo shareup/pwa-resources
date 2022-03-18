903df5b879555d0d15d696ed02e634b32cc72e3e
Nathan Herald <me@nathanherald.com>
2022-03-18 09:44:07 +0100

    Cleaned up the resources list, sorted

--- public/resources.ts
+++ public/resources.ts
@@ -9,53 +9,48 @@ export const resources: Resource[] = [
   {
     url: new URL('https://github.com/matthewp/ocean'),
     title: 'Ocean',
-    desc: 'Web component server-side rendering',
-    categories: ['Web Components', 'SSR', 'GitHub']
+    desc:
+      'Web component HTML rendering that runs on the server, in a browser worker, and includes hydration.',
+    categories: ['Web Components', 'SSR']
   },
   {
     url: new URL('https://preactjs.com/guide/v10/progressive-web-apps'),
     title: 'Progressive Web Apps Guide for Preact',
     desc:
-      'Preact is an excellent choice for Progressive Web Apps that wish to load and become interactive quickly. Preact CLI codifies this into an instant build tool that gives you a PWA with a 100 Lighthouse score right out of the box.',
+      '“Preact is an excellent choice for Progressive Web Apps that wish to load and become interactive quickly. Preact CLI codifies this into an instant build tool that gives you a PWA with a 100 Lighthouse score right out of the box.”',
     categories: ['Preact']
   },
   {
     url: new URL('https://create-react-app.dev/docs/making-a-progressive-web-app/'),
     title: 'Making a Progressive Web App with Create React App',
     desc:
-      "If you start a new project using one of the PWA custom templates, you'll get a src/service-worker.js file that serves as a good starting point for an offline-first service worker.",
+      "“If you start a new project using one of the PWA custom templates, you'll get a src/service-worker.js file that serves as a good starting point for an offline-first service worker.”",
     categories: ['React', 'Project Templates']
   },
   {
     url: new URL('https://wmr.dev'),
     title: 'WMR',
-    desc: 'Modern web app development without the wait',
+    desc: 'An all-in-one development tool for building PWAs with Preact.',
     categories: ['Preact', 'SSR', 'Project Templates']
   },
   {
     url: new URL('https://lit.dev'),
     title: 'Lit',
-    desc: 'Simple. Fast. Web Components.',
+    desc: 'A package for building “Simple. Fast. Web Components.”',
     categories: ['Web Components']
   },
   {
     url: new URL('https://github.com/lit/lit/tree/main/packages/labs/ssr#lit-labsssr'),
     title: '@litlabs/ssr',
-    desc: 'A package for server-side rendering Lit templates and components',
-    categories: ['SSR', 'Web Components', 'GitHub']
+    desc: '“A package for server-side rendering Lit templates and components.”',
+    categories: ['SSR', 'Web Components']
   },
   {
     url: new URL('https://frontendchecklist.io'),
     title: 'The Front-End Checklist',
     desc:
-      'The Front-End Checklist Application is perfect for modern websites and meticulous developers!',
+      '“The Front-End Checklist Application is perfect for modern websites and meticulous developers!”',
     categories: ['Checklists']
-  },
-  {
-    url: new URL('https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps'),
-    title: 'Progressive web apps (PWAs) on MDN',
-    desc: 'PWA how-to guides and other documentation on developer.mozilla.org.',
-    categories: ['Overview']
   },
   {
     url: new URL('https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Introduction'),
@@ -66,7 +61,7 @@ export const resources: Resource[] = [
   },
   {
     url: new URL('https://web.dev/progressive-web-apps/'),
-    title: 'Progressive Web Apps on web.dev',
+    title: 'Progressive Web Apps',
     desc:
       "You'll learn what makes a Progressive Web App special, how they can affect your business, and how to build them.",
     categories: ['Getting Started', 'Highlights']
@@ -81,7 +76,7 @@ export const resources: Resource[] = [
     url: new URL('https://developers.google.com/web/tools/workbox'),
     title: 'Workbox',
     desc:
-      'Workbox is a set of libraries that can power a production-ready service worker for your Progressive Web App.',
+      '“Workbox is a set of libraries that can power a production-ready service worker for your Progressive Web App.”',
     categories: ['Service Workers']
   },
   {
@@ -90,50 +85,51 @@ export const resources: Resource[] = [
     ),
     title: 'A Beginner’s Guide To Progressive Web Apps from Smashing Magazine',
     desc:
-      'This articles look into recent advancements, as of 2015, in the browser and the opportunities we, as developers, have to build a new generation of web apps. There is still a lot of valuable information here.',
+      'This articles look into the new, as of 2015, opportunities we have to build a new generation of web apps. There is still a lot of valuable information here.',
     categories: ['Getting Started', ':old']
   },
   {
     url: new URL('https://webaccessibilitychecklist.com'),
     title: 'Web Accessibility Checklist',
-    desc: 'A checklist for creating accessible websites and web applications.',
+    desc: '“A checklist for creating accessible websites and web applications.”',
     categories: ['Checklists', 'Accessibility']
   },
   {
     url: new URL('https://www.pwastats.com'),
     title: 'PWA Stats',
-    desc: 'A community-driven list of stats and news related to Progressive Web Apps by Cloud Four',
+    desc:
+      '“A community-driven list of stats and news related to Progressive Web Apps by Cloud Four.”',
     categories: ['Advocacy']
   },
   {
     url: new URL('https://blog.pusher.com/introduction-progressive-web-apps/'),
-    title: 'Introduction to Progressive Web Applications by Pusher',
-    desc: 'A high-level overview of PWAs from 2018.',
-    categories: [':old']
+    title: 'Introduction to Progressive Web Applications',
+    desc: 'A high-level overview of PWAs from Pusher, written in 2018.',
+    categories: ['Getting Started', ':old']
   },
   {
     url: new URL('https://www.udacity.com/course/intro-to-progressive-web-apps--ud811'),
-    title: 'Intro to Progressive Web Apps by Google on Udacity',
+    title: 'Intro to Progressive Web Apps Course by Google',
     desc:
-      'In this course you’ll get started working on your very first Progressive Web App (PWA) - a web app that can take advantage of many of the features native applications have enjoyed.',
-    categories: ['Getting Started', 'Courses']
+      '“In this course you’ll get started working on your very first Progressive Web App (PWA) - a web app that can take advantage of many of the features native applications have enjoyed.”',
+    categories: ['Courses']
   },
   {
     url: new URL(
       'https://www.twilio.com/blog/2016/02/web-powered-sms-inbox-with-service-worker-push-notifications.html'
     ),
-    title: 'Web Powered SMS Inbox with Service Worker: Push Notifications by Twilio',
+    title: 'Web Powered SMS Inbox with Service Worker: Push Notifications',
     desc:
-      'Recaps building a web application that I can use as a fully featured SMS messaging application for a Twilio number, from 2016.',
+      'Recaps building a web application: a fully featured SMS messaging application for a Twilio number, from 2016.',
     categories: ['Service Workers', ':old']
   },
   {
     url: new URL(
       'https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers'
     ),
-    title: 'Using Service Workers on MDN',
+    title: 'Using Service Workers',
     desc:
-      'This article provides information on getting started with service workers, including basic architecture, registering a service worker, the install and activation process for a new service worker, updating your service worker, cache control and custom responses, all in the context of a simple app with offline functionality.',
+      '“This article provides information on getting started with service workers, including basic architecture, registering a service worker, the install and activation process for a new service worker, updating your service worker, cache control and custom responses, all in the context of a simple app with offline functionality.”',
     categories: ['Service Workers', 'Offline']
   },
   {
@@ -144,41 +140,41 @@ export const resources: Resource[] = [
   },
   {
     url: new URL('https://www.speedcurve.com/blog/pwa-performance/'),
-    title: 'Validating PWA Performs while Offline with WebPageTest',
+    title: 'Checking PWA Performance with WebPageTest',
     desc: 'An example script for WebPageTest to verify a PWA loads while offline.',
     categories: ['Offline']
   },
   {
     url: new URL('https://github.com/koddr/a2hs.js'),
-    title: 'a2hs.js on GitHub',
-    desc: 'Add progressive web application (PWA) to Home Screen on iOS',
+    title: 'a2hs.js',
+    desc: 'A package to help add PWAs to the Home Screen on iOS.',
     categories: ['Installation']
   },
   {
     url: new URL('https://github.com/hemanth/awesome-pwa'),
-    title: 'awesome-pwa on GitHub',
-    desc: 'Useful resources for creating Progressive Web Apps',
-    categories: []
+    title: 'awesome-pwa',
+    desc: '”Useful resources for creating Progressive Web Apps.”',
+    categories: ['Advocacy']
   },
   {
     url: new URL('https://github.com/TalAter/UpUp'),
-    title: 'UpUp - Kickstarting the Offline First Revolution – on GitHub',
+    title: 'UpUp - Kickstarting the Offline First Revolution',
     desc:
-      "UpUp is a tiny JavaScript library that makes sure your users can always access your site's content, even when they're on a plane, in an elevator, or 20,000 leagues under the sea.",
+      "“UpUp is a tiny JavaScript library that makes sure your users can always access your site's content, even when they're on a plane, in an elevator, or 20,000 leagues under the sea.”",
     categories: ['Offline']
   },
   {
     url: new URL('https://github.com/ali-master/pwa-badge'),
-    title: 'PWA Badge on GitHub',
+    title: 'PWA Badge',
     desc:
-      'Like Native Apps, The PWA Badge API allows installed web apps to set an application-wide badge on the app icon.',
+      '“Like Native Apps, The PWA Badge API allows installed web apps to set an application-wide badge on the app icon.”',
     categories: ['Installation']
   },
   {
     url: new URL('https://simplepwa.com'),
     title: 'Simple Progressive Web App (PWA) Template',
     desc:
-      'Simple PWA is a Progressive Web App template that provides the minimum file structure needed to create a PWA—a reliable and installable web application.',
+      '“Simple PWA is a Progressive Web App template that provides the minimum file structure needed to create a PWA—a reliable and installable web application.”',
     categories: ['Project Templates', 'Offline']
   },
   {
@@ -189,37 +185,36 @@ export const resources: Resource[] = [
   },
   {
     url: new URL('https://www.pluralsight.com/courses/web-apps-progressive-getting-started'),
-    title: 'Getting Started with Progressive Web Apps on Pluralsight',
+    title: 'Getting Started with Progressive Web Apps Course',
     desc:
-      "A for-pay course to create a 'Car Deals' website using techniques and technologies to transform it from a simple web page into a progressive web app.",
+      "A course to create a 'Car Deals' website using techniques and technologies to transform it from a simple web page into a progressive web app.",
     categories: ['Courses', 'Paid']
   },
   {
     url: new URL('https://www.pluralsight.com/courses/progressive-web-app-fundamentals'),
-    title: 'Progressive Web App Fundamentals on Pluralsight',
-    desc: 'A for-pay course to learn to supercharge web apps so they can compete with native apps',
+    title: 'Progressive Web App Fundamentals Course',
+    desc: 'A course to learn to supercharge web apps so they can compete with native apps',
     categories: ['Courses', 'Paid']
   },
   {
     url: new URL('https://www.udemy.com/course/progressive-web-apps/'),
-    title: 'Progressive Web Apps: The Concise PWA Masterclass on Udemy',
+    title: 'Progressive Web Apps: The Concise PWA Masterclass Course',
     desc:
-      'Everything you need to build Progressive Web Apps from Scratch, or upgrade your existing Web Apps to PWAs.',
+      '“Everything you need to build Progressive Web Apps from Scratch, or upgrade your existing Web Apps to PWAs.”',
     categories: ['Courses', 'Paid']
   },
   {
     url: new URL('https://www.udemy.com/course/progressive-web-app-pwa-the-complete-guide/'),
     title: 'Progressive Web Apps (PWA) - The Complete Guide',
     desc:
-      'Build a Progressive Web App (PWA) that feels like an iOS & Android App, using Device Camera, Push Notifications and more',
+      '“Build a Progressive Web App (PWA) that feels like an iOS & Android App, using Device Camera, Push Notifications and more.”',
     categories: ['Courses', 'Paid']
   },
   {
     url: new URL('https://www.aaron-gustafson.com/notebook/pwa-qa/'),
     title: 'PWA Q&A by Aaron Gustafson',
-    desc:
-      'A collection of questions and answers by Aaron which covers many topics related to PWAs.',
-    categories: ['Highlights']
+    desc: 'A collection of questions and answers by Aaron which cover many topics related to PWAs.',
+    categories: ['Highlights', 'Advocacy']
   },
   {
     url: new URL('https://web.dev/app-like-pwas/'),
@@ -274,13 +269,6 @@ export const resources: Resource[] = [
     categories: ['Courses', 'Paid', 'Debugging']
   },
   {
-    url: new URL('https://chromeos.dev/en/web/desktop-progressive-web-apps'),
-    title: 'Desktop Progressive Web Apps',
-    desc:
-      '“Desktop PWAs are a great cross-platform, cross-browser way to build apps with a security model that centers user safety and privacy and get built-in sharing with the web’s inherent linkability.”',
-    categories: ['Installation', ':unsure']
-  },
-  {
     url: new URL('https://www.davrous.com/2019/10/18/myth-busting-pwas-the-new-edge-edition/'),
     title: 'Myth Busting PWAs',
     desc: '“By busting 9 Myths on PWAs, we’ll see that PWAs are stronger than ever.”',
@@ -290,9 +278,19 @@ export const resources: Resource[] = [
     url: new URL('https://en.wikipedia.org/wiki/Progressive_web_application'),
     title: 'Progressive web application on Wikipedia',
     desc: 'The Wikipedia page for PWAs has a summary of their history and some of the related tech',
-    categories: ['Overview']
+    categories: ['Getting Started']
   }
-]
+].sort((l, r) => {
+  const lTitle = l.title.toLowerCase().replace(/^the\s*/, '').replace(/^@/, '')
+  const rTitle = r.title.toLowerCase().replace(/^the\s*/, '').replace(/^@/, '')
+
+  if (lTitle < rTitle) {
+    return -1
+  } else if (lTitle > rTitle) {
+    return 1
+  }
+  return 0
+})
 
 export const slugsToCategory: Map<string, string> = new Map()
 export const categoryToSlugs: Map<string, string> = new Map()
@@ -308,5 +306,3 @@ for (const res of resources) {
 }
 
 export const categories = Array.from(categoryToSlugs.keys()).sort((a, b) => b.length - a.length)
-
-// TODO: is this 2018 course still good? https://www.udemy.com/course/learn-to-build-progressive-web-apps-using-javascript/
