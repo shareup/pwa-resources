cb770e050c6150e4166ecf93eefa6c0da3a3c6c8
Nathan Herald <me@nathanherald.com>
2022-03-21 14:44:49 +0100

    We need a slug for getting-started or else the URL won't route

--- public/resources.ts
+++ public/resources.ts
@@ -299,10 +299,11 @@ export const categoryToSlugs: Map<string, string> = new Map()
 for (const res of resources) {
   for (const col of res.categories) {
     if (col.startsWith(':')) { continue }
-    if (col === 'Getting Started') { continue }
 
     const slug = col.toLowerCase().replace(/\s+/, '-')
     slugsToCategory.set(slug, col)
+
+    if (col === 'Getting Started') { continue }
     categoryToSlugs.set(col, slug)
   }
 }
