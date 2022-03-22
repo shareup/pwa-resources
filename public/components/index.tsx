df21a354e068d05179890251fd170a1de99a3074
Nathan Herald <me@nathanherald.com>
2022-03-22 15:55:14 +0100

    Fix linting

--- public/components/index.tsx
+++ public/components/index.tsx
@@ -1,10 +1,10 @@
 import type { FunctionalComponent } from 'preact'
 import { Layout } from '../components/layout'
 import { categories, categoryToSlugs } from '../resources'
-import styles from './index.module.css'
 import { Featured } from './featured'
 import { GettingStartedAndNav } from './gs-and-nav'
 import { Header } from './header'
+import styles from './index.module.css'
 
 export const Index: FunctionalComponent = () => {
   return (
